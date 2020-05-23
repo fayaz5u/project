97cd09ecce388b16e3bf434095240a8dc7c4b3e6
fayaz5u <fayazkareemkhan@gmail.com>
2020-05-23 18:17:59 +0530

    Added by khan

---
+++ src/main/java/Org/tcs/fusoproject/App.java
@@ -0,0 +1,13 @@
+package Org.tcs.fusoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
